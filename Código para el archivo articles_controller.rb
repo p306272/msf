bc9fcfb93f570c5ee95b901668f01a911cd5fe38
p306272 <marpaez54@hotmail.com>
2016-08-10 21:29:01 -0500

    Update Código para el archivo articles_controller.rb
    primeros cambios al archivo

--- Código para el archivo articles_controller.rb
+++ Código para el archivo articles_controller.rb
@@ -1,6 +1,12 @@
 def create
   @article = Article.new(params[:article])
+ class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
  
+  def new
+  end
   @article.save
   redirect_to @article
 end
