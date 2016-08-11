e34e580ce7926d31cf313cbdcde0a60cc9391a18
p306272 <marpaez54@hotmail.com>
2016-08-10 21:57:17 -0500

    Update Código para el archivo articles_controller.rb
    Se crea el índice para cada registro

--- Código para el archivo articles_controller.rb
+++ Código para el archivo articles_controller.rb
@@ -1,9 +1,15 @@
 def create
   @article = Article.new(params[:article])
  class ArticlesController < ApplicationController
+   
+   def index
+    @articles = Article.all
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
+ 
  
   def new
   end
